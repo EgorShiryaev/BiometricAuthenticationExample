cdaf9c80d44f0e46b92fa31639824b1cc7a007c2
EgorShiryaev <shiryaevvyatsu@gmail.com>
2022-10-05 17:04:15 +0300

    init_repository

---
+++ ios/biometric_auth_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  biometric_auth_example
+//
+//  Created by Shiryaev Egor on 05.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
